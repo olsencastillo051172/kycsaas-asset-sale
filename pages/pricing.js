a1f07a88d267e3ea2be225316d5c37cf775ce6ac
olsencastillo051172 <olsencastillo92@gmail.com>
2025-12-05 11:52:23 -0500

    Add Pricing component with pricing options

---
+++ pages/pricing.js
@@ -0,0 +1,12 @@
+export default function Pricing() {
+  return (
+    <section>
+      <h2>Pricing</h2>
+      <ul>
+        <li>Starter – $149/mo</li>
+        <li>Growth – $499/mo</li>
+        <li>Enterprise – Custom</li>
+      </ul>
+    </section>
+  );
+}
