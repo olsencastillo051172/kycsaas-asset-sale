ddcf389a7e50b53370b3c2338f293652b2ff6304
olsencastillo051172 <olsencastillo92@gmail.com>
2025-12-05 11:50:51 -0500

    Create Product component with overview content
    Add a Product component with an overview section.

---
+++ pages/product.js
@@ -0,0 +1,8 @@
+export default function Product() {
+  return (
+    <section>
+      <h2>Product Overview</h2>
+      <p>End-to-end KYC/AML flow: ID, selfie, sanctions screening, risk scoring.</p>
+    </section>
+  );
+}
